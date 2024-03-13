9241133f0f3b2c37aa0074c9e617c9e5d7c08fce
HQI11068 <HQI11068@educastur.es>
2024-03-13 19:09:49 +0100

    COMMIT_tablas1

--- BASE2DAVIDDANIEL.sql
+++ BASE2DAVIDDANIEL.sql
@@ -3,3 +3,26 @@ INICIO DEL PROYECTO
 comit de prueba
 
 comit de prueba 2
+
+/* CREACION DE TABLAS */
+
+CREATE TABLE usuario (
+    id INT PRIMARY KEY,
+    nombre VARCHAR(100),
+    apellidos VARCHAR(100),
+    username VARCHAR(50),
+    correo VARCHAR(100),
+    clave_cifrada VARCHAR(100)
+);
+
+CREATE TABLE alumno (
+    id INT PRIMARY KEY,
+    usuario_id INT,
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+);
+
+CREATE TABLE profesor (
+    id INT PRIMARY KEY,
+    usuario_id INT,
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+);
